b0f42c20c766097df2b8afeb8fbae3c642209de9
fabing <1349212501@qq.com>
2022-03-26 22:51:36 +0800

    add oneshot experiments batch processing script

---
+++ run_oneshot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
+do
+  echo constraint: $constraint
+  python search.py --constraints $constraint --checkpoints ./checkpoints/oneshot/mobilenet/contraints-$constraint.json --model-path ./checkpoints/oneshot/mobilenet/contraints-$constraint.onnx
+done;
