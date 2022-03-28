bd57f0180ab66196b26284464becc98a79491856
fabing <1349212501@qq.com>
2022-03-28 18:02:22 +0800

    add retrain module

--- run_oneshot.sh
+++ run_oneshot.sh
@@ -2,17 +2,19 @@
 for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
 do
   echo constraint: $constraint
-#  python train.py -net mobilenet -gpu --arc-checkpoint ./checkpoints/oneshot/mobilenet/contraints-$constraint.json
-#  search
   model=mobilenet
-  dir=./checkpoints/oneshot/"${model}"/logfunction
+  lossType=logfunction
+  dir=./checkpoints/oneshot/"${model}"/"${lossType}"
+  #  search
   mkdir -p "${dir}"
-  python search.py \
-  --net "${model}" \
-  --gpu \
-  --worker-id 1 \
-  --constraint $constraint \
-  --checkpoints "${dir}"/contraints-$constraint.json \
-  --model-path "${dir}"/contraints-$constraint.onnx
+#  python search.py \
+#  --net "${model}" \
+#  --gpu \
+#  --worker-id 1 \
+#  --constraint $constraint \
+#  --checkpoints "${dir}"/contraints-$constraint.json \
+#  --model-path "${dir}"/contraints-$constraint.onnx
+#  retrain
+  python train.py --net "${model}" --gpu --worker-id 2 --loss-type "${lossType}" --arc-checkpoint "${dir}"/contraints-$constraint.json
 
 done;
