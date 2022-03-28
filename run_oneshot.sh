ca81cf61adf3fc8d396a7c11b9702232cc7612a4
fabing <1349212501@qq.com>
2022-03-28 16:24:08 +0800

    remove comments

--- run_oneshot.sh
+++ run_oneshot.sh
@@ -3,16 +3,12 @@ for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
 do
   echo constraint: $constraint
 #  python train.py -net mobilenet -gpu --arc-checkpoint ./checkpoints/oneshot/mobilenet/contraints-$constraint.json
-#  search mobilenet
-#  python search.py \
-#  --constraints $constraint \
-#  --checkpoints ./checkpoints/oneshot/mobilenet/logfunction/contraints-$constraint.json \
-#  --model-path ./checkpoints/oneshot/mobilenet/logfunction/contraints-$constraint.onnx
-#  search resnet18
-  dir=./checkpoints/oneshot/resnet18/logfunction
+#  search
+  model=mobilenet
+  dir=./checkpoints/oneshot/"${model}"/logfunction
   mkdir -p "${dir}"
   python search.py \
-  --net resnet18 \
+  --net "${model}" \
   --gpu \
   --worker-id 1 \
   --constraint $constraint \
