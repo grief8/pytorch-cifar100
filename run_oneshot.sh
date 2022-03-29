8628c9bfb4e10e92b7a76149f7815f1164be3f2b
Fabing Li <1349212501@qq.com>
2022-03-29 11:35:29 +0800

    search resnet50

--- run_oneshot.sh
+++ run_oneshot.sh
@@ -2,19 +2,20 @@
 for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
 do
   echo constraint: $constraint
-  model=resnet18
+  model=resnet50
   lossType=logfunction
   dir=./checkpoints/oneshot/"${model}"/"${lossType}"
   #  search
   mkdir -p "${dir}"
-#  python search.py \
-#  --net "${model}" \
-#  --gpu \
-#  --worker-id 1 \
-#  --constraint $constraint \
-#  --arc-checkpoint "${dir}"/contraints-$constraint.json \
-#  --model-path "${dir}"/contraints-$constraint.onnx
+  python search.py \
+  --net "${model}" \
+  --gpu \
+  --pretrained \
+  --worker-id 1 \
+  --constraint $constraint \
+  --arc-checkpoint "${dir}"/contraints-$constraint.json \
+  --model-path "${dir}"/contraints-$constraint.onnx
 #  retrain
-  python train.py --net "${model}" --gpu --worker-id 4 --loss-type "${lossType}" --arc-checkpoint "${dir}"/contraints-$constraint.json
+#  python train.py --net "${model}" --gpu --worker-id 4 --loss-type "${lossType}" --arc-checkpoint "${dir}"/contraints-$constraint.json
 
 done;
