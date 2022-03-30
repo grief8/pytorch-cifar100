9a601642e4650ff26db6a8f52be1a0a850f9fe1d
Fabing Li <1349212501@qq.com>
2022-03-30 18:24:07 +0800

    add backend running

--- run_oneshot.sh
+++ run_oneshot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
-do
-  model=resnet18
-  lossType=sqrt
+function run() {
+  constraint=$1
+  wid=$(echo "scale=0;  ($1*10)%8/1" | bc)
+  model=resnet50
+  lossType=l2regularization
+  echo start "${model}" "${lossType}" $constraint
   dir=./checkpoints/oneshot/"${model}"/"${lossType}"
   #  search
   mkdir -p "${dir}"
@@ -10,9 +12,9 @@ do
   --net "${model}" \
   --gpu \
   --pretrained \
-  --worker-id 0 \
+  --worker-id $wid \
   --epochs 200 \
-  --batch-size 2048 \
+  --batch-size 512 \
   --loss-type "${lossType}" \
   --constraint $constraint \
   --arc-checkpoint "${dir}"/contraints-$constraint.json \
@@ -22,9 +24,13 @@ do
   --net "${model}" \
   --gpu \
   --pretrained \
-  --worker-id 3 \
-  --batch-size 1024 \
+  --worker-id $wid \
+  --batch-size 512 \
   --loss-type "${lossType}" \
-  --arc-checkpoint "${dir}"/contraints-$constraint.json &
+  --arc-checkpoint "${dir}"/contraints-$constraint.json
+}
 
+for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
+do
+  run $constraint &
 done;
