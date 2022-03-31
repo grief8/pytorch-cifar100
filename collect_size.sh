95836672d8b63229e3b5bb9ec3a5ed27e1795b9b
fabing <1349212501@qq.com>
2022-03-31 11:03:24 +0800

    enter the parameter through cmd line

--- collect_size.sh
+++ collect_size.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for constraint in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0;
 do
-  model=resnet18
-  lossType=logfunction
+  model=$1
+  lossType=$2
   dir=./checkpoints/oneshot/"${model}"/"${lossType}"
 #  echo $constraint
   python eval_models.py --net "${model}" --arc-checkpoint "${dir}"/contraints-$constraint.json
